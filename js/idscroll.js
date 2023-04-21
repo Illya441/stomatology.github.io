db34db5b286be5fa1e5c9bb8ccebab386bcb3912
Illya <84741257+Illya441@users.noreply.github.com>
2023-04-21 16:12:21 +0300

    first commit

---
+++ js/idscroll.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var headerHeight = $('header').outerHeight();// Target your header navigation here 
+    $('.menu_link').click(function(e) {
+        var linkHref = $(this).attr('href'); 
+        
+        console.log(headerHeight)
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top - headerHeight // Add it to the calculation here
+        }, 1300);
+        e.preventDefault();
+      });
+    });
+    //отвечает чтобы хедер уменьшался
+    
+    const header = document.querySelector('.header');
+    const threshold = 20; // change this to adjust when the header starts to shrink
+    let scrolled = 0;
+    
+    window.addEventListener('scroll', () => {
+      scrolled = window.scrollY;
+      if (scrolled > threshold) {
+        header.classList.add('shrink');
+      } else {
+        header.classList.remove('shrink');
+      }
+    });
+    
---
+++ js/slider.js
@@ -0,0 +1,15 @@
+//  Initialize Swiper
+let swiper = new Swiper(".mySwiper", {
+//  Initialize Swiper
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    speed: 500, // Add this line to change the speed
+    
+});
