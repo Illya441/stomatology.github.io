11cf969725f2c8a50794c2ce34e74e3b08d392de
Illya <84741257+Illya441@users.noreply.github.com>
2023-04-22 02:29:13 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+//  Initialize Swiper
+let swiper = new Swiper(".mySwiper", {
+//  Initialize Swiper
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 800, // Add this line to change the speed
+    
+});
