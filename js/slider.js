ea8a78192bcb15412753c84c6010e66b22ea40b2
Illya <bernatskiy.i@gmail.com>
2023-04-28 20:00:36 +0300

    website redesign

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,13 @@
 //  Initialize Swiper
 let swiper = new Swiper(".mySwiper", {
 //  Initialize Swiper
-
+    loop: true, // добавьте эту строку для зацикливания слайдера
     pagination: {
       el: ".swiper-pagination",
     },
 
     autoplay: {
-      delay: 3000,
+      delay: 1500,
       disableOnInteraction: false,
     },
     speed: 800, // Add this line to change the speed
